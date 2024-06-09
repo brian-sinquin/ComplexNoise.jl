1b96b8cf7816742f04a8c1a921a551261a46554f
Brian SINQUIN <brian.sinquin@gmail.com>
2024-06-09 17:50:14 +0200

    Testing branches docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,18 @@
 using ComplexNoise
 using Documenter
 
-DocMeta.setdocmeta!(ComplexNoise, :DocTestSetup, :(using ComplexNoise); recursive=true)
+DocMeta.setdocmeta!(ComplexNoise, :DocTestSetup, :(using ComplexNoise); recursive = true)
 
 makedocs(;
-    modules=[ComplexNoise],
-    authors="Brian SINQUIN <brian.sinquin@gmail.com> and contributors",
-    sitename="ComplexNoise.jl",
-    format=Documenter.HTML(;
-        canonical="https://brian-sinquin.github.io/ComplexNoise.jl",
-        edit_link="master",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
+	modules = [ComplexNoise],
+	authors = "Brian SINQUIN <brian.sinquin@gmail.com> and contributors",
+	sitename = "ComplexNoise.jl",
+	format = Documenter.HTML(;
+		canonical = "https://brian-sinquin.github.io/ComplexNoise.jl",
+		edit_link = "master",
+		assets = String[],
+	),
+	pages = ["🏡 Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/brian-sinquin/ComplexNoise.jl",
-    devbranch="master",
-)
+deploydocs(; repo = "github.com/brian-sinquin/ComplexNoise.jl", devbranch = "master")
